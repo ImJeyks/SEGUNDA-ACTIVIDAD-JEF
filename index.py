77d8110114a4a0dc37f6441e06a10bf9a9b1beb3
jeferson6329 <jeferson.lugo1012@gmail.com>
2026-02-12 01:28:31 +0000

    version2

--- index.py
+++ index.py
@@ -2,4 +2,16 @@ name = input("Digite su nombre: ")
 edad = int(input("Digite su edad: "))
 peso = int(input("Digite su peso: "))
 
-print ("YOYOYOU MS. " + name )
+if edad >= 50:
+    categoria = "una persona de edad"
+elif edad >= 30:
+    categoria = "un adulto"
+elif edad >= 18:
+    categoria = "joven"
+else:
+    categoria = "menor de edad"
+
+print("Hola" +name +", su edad es "+edad+ " años, usted es "+ categoria)
+
+
+
