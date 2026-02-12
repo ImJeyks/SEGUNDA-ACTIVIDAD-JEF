3f6ffbf0da488fd63227ca68419b91fb7647dbc3
ImJeyks <juancetina699@gmail.com>
2026-02-12 00:30:46 +0000

    Version2

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 name = input("Digite su nombre: ")
 edad = int(input("Digite su edad: "))
-profesion = input("Digite su profesion: ")
 peso = int(input("Digite su peso: "))
 
-print ("YOYOYOU MR. " + name )
+print ("YOYOYOU MS. " + name )
